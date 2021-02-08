d081cb54d23b1f9bb57fa07824bc15237f861469
AndersonShen2020 <shen.andersonshen@gmail.com>
2021-02-08 23:10:06 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-test-2020/" : "/",
+// module.exports = {
+//   publicPath:
+//     process.env.NODE_ENV === "production"
+//       ? "/vue-test-2020/" // vue-test-2020 為 repo 名稱
+//       : "./dist/",
+// };
+
+const config = {
+  publicPath: "",
 };
+
+module.exports = config;
