2a516643a5897f3c7dd7134a09be8f50b2383d62
AndersonShen2020 <shen.andersonshen@gmail.com>
2021-02-09 00:14:18 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,12 @@
 // };
 
 const config = {
+  filenameHashing: false,
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/vue-test-2020/" // vue-test-2020 為 repo 名稱
-      : "./dist/",
+      ? //   ? "/vue-test-2020/" // vue-test-2020 為 repo 名稱
+        "./"
+      : "/",
 };
 
 module.exports = config;
