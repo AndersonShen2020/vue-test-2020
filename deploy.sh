1b3477d39401331f59c49b47e63cab51174c9a09
AndersonShen2020 <shen.andersonshen@gmail.com>
2021-02-08 18:00:56 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,4 +14,4 @@ git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
 git push -f https://github.com/AndersonShen2020/vue-test-2020.git master:gh-pages
 # //將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-cd -
+cd - 
