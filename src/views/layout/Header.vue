4d3d3118090ae38d41f1e2908e2b95936db28f5c
AndersonShen2020 <shen.andersonshen@gmail.com>
2021-02-17 17:30:36 +0800

    增加router-link

--- src/views/layout/Header.vue
+++ src/views/layout/Header.vue
@@ -6,7 +6,8 @@ export default {};
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/Weather">天氣預報</router-link>
+    <router-link to="/Table">Element-Plus Table測試</router-link> |
+    <router-link to="/Weather">三十六小時天氣預報</router-link>
   </div>
 </template>
 
