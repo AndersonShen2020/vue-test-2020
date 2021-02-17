7c22e19bb35a3abd21bc05d31ea43d8ba343edac
AndersonShen2020 <shen.andersonshen@gmail.com>
2021-02-17 17:31:02 +0800

    Table測試資料

---
+++ src/api/Table.js
@@ -0,0 +1,55 @@
+const tableData = [
+  {
+    date: "2016-05-02 01",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1518 弄",
+  },
+  {
+    date: "2016-05-04 02",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1517 弄",
+  },
+  {
+    date: "2016-05-01 03",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1519 弄",
+  },
+  {
+    date: "2016-05-03 04",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1516 弄",
+  },
+  {
+    date: "2016-05-03 05",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1516 弄",
+  },
+  {
+    date: "2016-05-03 06",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1516 弄",
+  },
+  {
+    date: "2016-05-03 07",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1516 弄",
+  },
+  {
+    date: "2016-05-03 08",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1516 弄",
+  },
+  {
+    date: "2016-05-03 09",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1516 弄",
+  },
+  {
+    date: "2016-05-03 10",
+    name: "王小虎",
+    address: "上海市普陀区金沙江路 1516 弄",
+  },
+];
+
+export const getTableData = tableData;
+export const getTableDataLength = tableData.length
