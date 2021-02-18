98d3d1d8ddc094b983712dcb70df0da6b28fb1bd
AndersonShen2020 <shen.andersonshen@gmail.com>
2021-02-19 01:54:02 +0800

    todoList 試做

---
+++ src/views/TodoList/index.vue
@@ -0,0 +1,26 @@
+<script>
+import { onMounted, reactive, ref, computed } from "vue";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+    /** 選取資料狀態 all , active , done */
+    const filter = "all";
+    const list = computed(() => {
+      return store.getters.filterList(filter);
+    });
+    return {
+      filter,
+      list,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>TodoList Page</h1>
+  <p>show: {{ filter }}</p>
+  <div>{{ list }}</div>
+</template>
+
+<style></style>
